6f68cb51e6f4990883a5f4748442a60e64598658
mt-code <matt@mtcode.co.uk>
2022-02-10 15:37:29 +0000

    fix: Header adjustments when opening

--- wordpress/wp-content/themes/astra-child/assets/js/main.js
+++ wordpress/wp-content/themes/astra-child/assets/js/main.js
@@ -2,6 +2,10 @@ var $ = jQuery;
 
 $(document).ready(function() {
     $('.gp-site-header__menu-toggle').on('click', function() {
+        if (!$(this).hasClass('is-active')) {
+            window.scrollTo(0, 0);
+        }
+
         $(this).toggleClass('is-active');
         $('.gp-mobile-header').toggleClass('is-active');
         $('body').toggleClass('stop-scrolling');
--- wordpress/wp-content/themes/astra-child/functions.php
+++ wordpress/wp-content/themes/astra-child/functions.php
@@ -8,7 +8,7 @@ function goodproduct_init() {
     ));
 
     wp_enqueue_style('goodproduct', get_stylesheet_directory_uri() . '/assets/css/main.css', array(), '1.0.5');
-    wp_enqueue_script('goodproduct-script', get_stylesheet_directory_uri() . '/assets/js/main.js', array('jquery'), '1.0.2');
+    wp_enqueue_script('goodproduct-script', get_stylesheet_directory_uri() . '/assets/js/main.js', array('jquery'), '1.0.3');
 }
 
 if( function_exists('acf_add_options_page') ) {
